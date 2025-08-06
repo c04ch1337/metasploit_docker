0523801d00a9f152a8f1c2d2e6f5441d1456ebaa
C04CH <88205891+c04ch1337@users.noreply.github.com>
2025-08-06 10:07:18 -0500

    Create start_console.sh

---
+++ examples/start_console.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+// Purpose: Launch the Metasploit Console (msfconsole) in a convenient way, possibly as part of a startup script or lab automation.
+
+echo "[+] Launching Metasploit Console..."
+msfconsole
