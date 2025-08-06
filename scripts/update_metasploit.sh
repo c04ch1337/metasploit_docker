cc751b9e9ac7adbd97c07a0db17b1f498e8df989
C04CH <88205891+c04ch1337@users.noreply.github.com>
2025-08-06 10:04:33 -0500

    Create update_metasploit.sh

---
+++ scripts/update_metasploit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+// Purpose: Update the Metasploit Framework and its modules (exploits, payloads, auxiliary, etc.) to the latest version. //
+
+echo "[+] Updating Metasploit Framework..."
+msfupdate
